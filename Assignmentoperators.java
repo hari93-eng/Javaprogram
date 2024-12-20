8a6852e9f327115eb21da49773b4ba246b820b71
hari93-eng <hsun4041@gmail.com>
2024-12-20 09:07:04 +0530

    Add files via upload

---
+++ Assignmentoperators.java
@@ -0,0 +1,11 @@
+package javaprograms;
+
+public class Assignmentoperator {
+	//Assignment operator with Arithmetic Operator
+
+	public static void main(String a[]) {
+		int c1=8,d=6;
+		System.out.println(c1+=3);//c1=c1+3
+		System.out.println(c1*=3);//c1=c1+3
+}
+}
