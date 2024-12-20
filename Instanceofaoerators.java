054cbda8b097c0383df070eb3c6b9e74ad414d80
hari93-eng <hsun4041@gmail.com>
2024-12-20 09:08:32 +0530

    Add files via upload

---
+++ Instanceofaoerators.java
@@ -0,0 +1,10 @@
+package javaprograms;
+
+public class Instanceofoperators {
+public static void main (String[] a) {
+	Operators op3=new Operators();
+	Operators op4=new Operators();
+	System.out.println((op3 instanceof Operators));
+	System.out.println((op4 instanceof Operators));	
+}
+}
