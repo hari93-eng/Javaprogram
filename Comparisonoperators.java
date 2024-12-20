27e988fbf109e7c2a9cfedf219c153835a214275
hari93-eng <hsun4041@gmail.com>
2024-12-20 09:08:06 +0530

    Add files via upload

---
+++ Comparisonoperators.java
@@ -0,0 +1,18 @@
+package javaprograms;
+
+public class Comparisonoperators {
+	//Comparison operators
+	public static void main(String a[]) {
+	int g=20,h=30,i=40;
+	//>-greater than
+	System.out.println((h>g));
+	//<-lesser than
+	System.out.println((h<g));
+	//==-equal to
+	System.out.println((h==g));
+	//>=-greater than equal to
+	System.out.println((h>=g));
+	//<=-lesser than equal to
+	System.out.println((h<=g));
+}
+}
