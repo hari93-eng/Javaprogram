7adc07691fe2561705ec9f6c9b0cede268e82329
hari93-eng <hsun4041@gmail.com>
2024-12-20 09:10:09 +0530

    Add files via upload

---
+++ Unaryoperators.java
@@ -0,0 +1,19 @@
+package javaprograms;
+
+public class unaryoperators {
+	//-Unary Operator(++)
+	public static void main(String a[]) {
+		int a1=25,b=8;
+	//Post Increment
+	System.out.println("% values is"+(a1++));
+	//Pre Increment
+	System.out.println("% values is"+(++a1));
+	
+	//PostDecrement
+			System.out.println("% values is"+(b--));
+			
+			//PreDecrement
+			System.out.println("% values is"+(--b));
+	
+}
+}
