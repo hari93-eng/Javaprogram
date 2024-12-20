f825072573af5c784f33298e8e527e1bf5a7d031
hari93-eng <hsun4041@gmail.com>
2024-12-20 09:09:20 +0530

    Add files via upload

---
+++ Logicaloperators.java
@@ -0,0 +1,14 @@
+package javaprograms;
+
+public class Logicaloperators {
+	public static void main(String a[]) {
+		//Logical Operator
+				//&&-and, ||-or, !-not
+					boolean j=true;
+					boolean k=false;
+					
+					System.out.println((j && k));
+					System.out.println((j||k));
+					System.out.println(!(j));
+	}
+}
