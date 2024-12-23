b87301dc9f71dbe75416ffe096ead49a6757e2ca
hari93-eng <hsun4041@gmail.com>
2024-12-23 09:20:30 +0530

    Add files via upload

---
+++ smallest3numbers.java
@@ -0,0 +1,23 @@
+package javaprograms;
+
+public class smallest3numbers {
+	public static void main(String[] args) {
+	int num1 = 22;
+	System.out.println("15");
+	int num2 = 15;
+	System.out.println("12");
+	int num3 = 12;
+	if(num1<num2 && num1<num3)
+	{
+		System.out.println("the first number is smallest number");
+	}
+	else if(num2<num1 && num2<num3){
+		System.out.println("the second number is the smallest number");
+	}
+	else 
+	{
+		System.out.println("the third number is the smallest number");
+	
+	}
+	}
+}
