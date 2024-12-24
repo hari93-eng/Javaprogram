2450b0ad8a4296fd71af4e0b5a215cdbb5d7c797
hari93-eng <hsun4041@gmail.com>
2024-12-24 08:30:00 +0530

    Add files via upload

---
+++ printsumofevevandoddnum.java
@@ -0,0 +1,20 @@
+package javaprograms;
+
+public class printevenandoddnumbers {
+	public static void main(String args[])
+	{
+		//while(condition)
+		//{statement:}
+		
+		int i=0;
+		int j=0;
+		while(i%2==0 && i<20)
+		while(j%1==0 && j<20)
+		{
+			System.out.println(i);
+			i+=2;
+			System.out.println(j);
+			j+=1;
+		}
+	}
+	}
